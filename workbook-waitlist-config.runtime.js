5bd3f14ec150c414b068ea1cff0842d2018106c9
Origin <origin@local>
2026-04-08 07:22:01 -0400

    Add workbook waitlist runtime config shell

---
+++ workbook-waitlist-config.runtime.js
@@ -0,0 +1,9 @@
+window.__THRIVING_DANCER_WORKBOOK_WAITLIST__ = window.__THRIVING_DANCER_WORKBOOK_WAITLIST__ || {
+  submitMode: 'placeholder',
+  endpoint: '',
+  successMode: 'inline-success',
+  redirectUrl: '',
+  toolLabel: 'not-configured',
+  privacyPolicyUrl: '',
+  successMessage: ''
+};
